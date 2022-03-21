7de526ed01a2e997ca5ff9e6526d9aeb8963f06d
X-DSI <xmandsi@gmail.com>
2022-03-21 14:52:18 +0530

    Basic Footer Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import { Fwrapper } from "./Footer.styled";
 
 const Footer = () => {
-  return <Fwrapper>Footer</Fwrapper>;
+  return (
+    <Fwrapper>
+      <p>&lt; / &gt; </p> <p>&nbsp; with &nbsp;</p>
+      <p> &#10084;</p>
+    </Fwrapper>
+  );
 };
 
 export default Footer;
--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 export const Fwrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 100vw;
 `;
